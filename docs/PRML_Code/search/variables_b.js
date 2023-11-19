d78ef904c6f1863d0824501961e4532dc06fe79d
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-11-19 14:50:16 +0800

    Deploy to GitHub Pages

--- docs/PRML_Code/search/variables_b.js
+++ docs/PRML_Code/search/variables_b.js
@@ -1,16 +1,17 @@
 var searchData=
 [
   ['max_0',['max',['../structdataset.html#a306b5ca364bf842a8ff5bbfc0b4d4a4b',1,'dataset']]],
-  ['mean_1',['mean',['../structdataset.html#a976925cd66ff0dcd419628463f580f1c',1,'dataset']]],
-  ['mean_5foverlapping_5fbboxes_2',['mean_overlapping_bboxes',['../namespacetrain__frcnn.html#ab16624b510ddf5781f5ca29b0bcc0d18',1,'train_frcnn']]],
-  ['metrics_3',['metrics',['../namespacetrain__frcnn.html#abcce662da2a75360795281c4d193ac56',1,'train_frcnn']]],
-  ['min_4',['min',['../structdataset.html#ad2e88d75f808e6d4e78d48bceb10c336',1,'dataset']]],
-  ['min_5ferror_5',['min_error',['../structintersection.html#ad958bc47a2a3d0dcc5f81359b2d22635',1,'intersection']]],
-  ['model_5fall_6',['model_all',['../namespacetrain__frcnn.html#ac54aa7a922342503e4b06386251443ef',1,'train_frcnn']]],
-  ['model_5fclassifier_7',['model_classifier',['../namespacetest__frcnn.html#a449d9ae4a4bbf41d089b1c9b6636b5da',1,'test_frcnn.model_classifier'],['../namespacetrain__frcnn.html#a449d9ae4a4bbf41d089b1c9b6636b5da',1,'train_frcnn.model_classifier']]],
-  ['model_5fclassifier_5fonly_8',['model_classifier_only',['../namespacetest__frcnn.html#a138a2d17c44a9cbb4141cef12f54a74e',1,'test_frcnn']]],
-  ['model_5fpath_9',['model_path',['../classkeras__frcnn_1_1config_1_1_config.html#a4d5bb0c360b13429f65cd327c8d0aa12',1,'keras_frcnn.config.Config.model_path'],['../namespacetest__frcnn.html#a4d5bb0c360b13429f65cd327c8d0aa12',1,'test_frcnn.model_path'],['../namespacetrain__frcnn.html#a4d5bb0c360b13429f65cd327c8d0aa12',1,'train_frcnn.model_path']]],
-  ['model_5fpath_5fregex_10',['model_path_regex',['../namespacetrain__frcnn.html#a8bd55e7ad00c7747317671f9c392d2a0',1,'train_frcnn']]],
-  ['model_5frpn_11',['model_rpn',['../namespacetest__frcnn.html#a005e2b7394511f344aa0289dbfa444c8',1,'test_frcnn.model_rpn'],['../namespacetrain__frcnn.html#a005e2b7394511f344aa0289dbfa444c8',1,'train_frcnn.model_rpn']]],
-  ['mu_12',['mu',['../structnorm__dist.html#a4df8d1d064e0269c44c34fd4a7a3b705',1,'norm_dist::mu'],['../structdataset.html#a4df8d1d064e0269c44c34fd4a7a3b705',1,'dataset::mu']]]
+  ['max_5fiter_1',['max_iter',['../structconfig__t.html#a53380b3b14c20bdfcb3bc9970c421231',1,'config_t']]],
+  ['mean_2',['mean',['../structdataset.html#a976925cd66ff0dcd419628463f580f1c',1,'dataset']]],
+  ['mean_5foverlapping_5fbboxes_3',['mean_overlapping_bboxes',['../namespacetrain__frcnn.html#ab16624b510ddf5781f5ca29b0bcc0d18',1,'train_frcnn']]],
+  ['metrics_4',['metrics',['../namespacetrain__frcnn.html#abcce662da2a75360795281c4d193ac56',1,'train_frcnn']]],
+  ['min_5',['min',['../structdataset.html#ad2e88d75f808e6d4e78d48bceb10c336',1,'dataset']]],
+  ['min_5ferror_6',['min_error',['../structintersection.html#ad958bc47a2a3d0dcc5f81359b2d22635',1,'intersection']]],
+  ['model_5fall_7',['model_all',['../namespacetrain__frcnn.html#ac54aa7a922342503e4b06386251443ef',1,'train_frcnn']]],
+  ['model_5fclassifier_8',['model_classifier',['../namespacetest__frcnn.html#a449d9ae4a4bbf41d089b1c9b6636b5da',1,'test_frcnn.model_classifier'],['../namespacetrain__frcnn.html#a449d9ae4a4bbf41d089b1c9b6636b5da',1,'train_frcnn.model_classifier']]],
+  ['model_5fclassifier_5fonly_9',['model_classifier_only',['../namespacetest__frcnn.html#a138a2d17c44a9cbb4141cef12f54a74e',1,'test_frcnn']]],
+  ['model_5fpath_10',['model_path',['../classkeras__frcnn_1_1config_1_1_config.html#a4d5bb0c360b13429f65cd327c8d0aa12',1,'keras_frcnn.config.Config.model_path'],['../namespacetest__frcnn.html#a4d5bb0c360b13429f65cd327c8d0aa12',1,'test_frcnn.model_path'],['../namespacetrain__frcnn.html#a4d5bb0c360b13429f65cd327c8d0aa12',1,'train_frcnn.model_path']]],
+  ['model_5fpath_5fregex_11',['model_path_regex',['../namespacetrain__frcnn.html#a8bd55e7ad00c7747317671f9c392d2a0',1,'train_frcnn']]],
+  ['model_5frpn_12',['model_rpn',['../namespacetest__frcnn.html#a005e2b7394511f344aa0289dbfa444c8',1,'test_frcnn.model_rpn'],['../namespacetrain__frcnn.html#a005e2b7394511f344aa0289dbfa444c8',1,'train_frcnn.model_rpn']]],
+  ['mu_13',['mu',['../structdataset.html#a4df8d1d064e0269c44c34fd4a7a3b705',1,'dataset::mu'],['../structnorm__dist.html#a4df8d1d064e0269c44c34fd4a7a3b705',1,'norm_dist::mu']]]
 ];
